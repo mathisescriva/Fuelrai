903a1b461cdb4b45f6c6c1112a89a2eda96749fe
mathisescriva <mathis.escriva@edu.ece.FR>
2025-01-29 12:15:01 +0100

    feat: ajout de la page d'accueil avec interface de sélection

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { MainLayout } from './layouts/MainLayout'
 import { KIDManager } from './components/KIDManager/KIDManager'
+import { Home } from './components/Home/Home'
 
 function App() {
   return (
-    <MainLayout>
-      <KIDManager />
-    </MainLayout>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/kid-analyzer" element={
+          <MainLayout>
+            <KIDManager />
+          </MainLayout>
+        } />
+        <Route path="/annual-reports" element={
+          <MainLayout>
+            <div className="p-4">
+              <h1 className="text-2xl font-bold">Analyseur de rapports annuels</h1>
+              <p className="mt-4">Cette fonctionnalité sera bientôt disponible.</p>
+            </div>
+          </MainLayout>
+        } />
+      </Routes>
+    </Router>
   );
 }
 
