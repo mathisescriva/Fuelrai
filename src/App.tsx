8716acb5a17a6885d4b8230d6338a48235f93945
mathisescriva <mathis.escriva@edu.ece.FR>
2025-02-12 17:07:50 +0100

    refactor: suppression des pages Home et AnnualReportAnalyzer pour ne garder que KID Analyser

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { MainLayout } from './layouts/MainLayout'
 import { KIDManager } from './components/KIDManager/KIDManager'
-import { Home } from './components/Home/Home'
-import AnnualReportAnalyzer from './components/AnnualReportAnalyzer/AnnualReportAnalyzer'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/kid-analyzer" element={
-          <MainLayout>
-            <KIDManager onUpload={(files) => console.log('Files uploaded:', files)} />
-          </MainLayout>
-        } />
-        <Route path="/annual-reports" element={
-          <MainLayout>
-            <AnnualReportAnalyzer />
-          </MainLayout>
-        } />
-      </Routes>
-    </Router>
+    <MainLayout>
+      <KIDManager onUpload={(files) => console.log('Files uploaded:', files)} />
+    </MainLayout>
   );
 }
 
